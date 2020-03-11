905663665b57e0b0c63a635d501e782da9eaa225
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-03-11 12:51:00 -0400

    added function to change html color based on time. added function to get time on start to remove delay

--- script.js
+++ script.js
@@ -1,19 +1,60 @@
 //military time 9am to 6pm. empty strings for user input
 var schedule = {
-    9: "",
-    10: "",
-    11: "",
-    12: "",
-    13: "",
-    14: "",
-    15: "",
-    16: "",
-    17: "",
-    18: "",
+  9: "",
+  10: "",
+  11: "",
+  12: "",
+  13: "",
+  14: "",
+  15: "",
+  16: "",
+  17: "",
+  18: "",
 }
-var timeKeep = setInterval(myTimer, 1000);
+var mt;
+var pt;
+const timeOpt = {
+  hour12: false,
+  hour: "numeric",
+  }
 
-function myTimer() {
-  var d = new Date();
-$("#timebox").html(d.toLocaleTimeString())
+var timeArray = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+var d;
+var timeKeep = setInterval(computeTimer, 1000);
+nodelay()
+
+function computeTimer() {
+  mt = new Date().toLocaleTimeString("en-US",timeOpt)
+  $("#miltime").html(mt)
+  if(pt !== mt){
+  timecolorchanger()
+  pt = mt;
+  }
+}
+
+function displayTimer() {
+  d = new Date();
+  $("#timebox").html(d.toLocaleTimeString())
+  $("#datebox").html(d.toLocaleDateString())
+}
+function nodelay(){
+  mt = new Date().toLocaleTimeString("en-US",timeOpt)
+  pt = mt
+  timecolorchanger()
+  $("#miltime").html(mt)
+}
+
+function timecolorchanger(){
+  for (var i = 0; i < timeArray.length; i++) {
+    var id = "#"+timeArray[i].toString();
+    if(timeArray[i]< mt){
+    $(id).css("background-color","grey")
+    }
+    else if(timeArray[i] > mt){
+      $(id).css("background-color","green")
+    }
+    else{
+      $(id).css("background-color","red")
+    }
+  }
 }
